e2861f946676f54c95e13e711d29825884ac90e7
Kye Bracey <kye.bracey@gmail.com>
2017-09-13 14:23:40 +0100

    Adds TouchableNativeFeedback to each grid area (no onPress event yet)

--- gen/components/board.js
+++ gen/components/board.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, ListView, View } from 'react-native';
+import { ListView, Text, TouchableNativeFeedback, View } from 'react-native';
 import styles from '../styles/board';
 
 class Board extends React.Component {
@@ -7,7 +7,7 @@ class Board extends React.Component {
     super()
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      dataSource: ds.cloneWithRows(['0', '1', '2', '3', '4', '5', '6', '7', '8']),
+      dataSource: ds.cloneWithRows([0, 1, 2, 3, 4, 5, 6, 7, 8]),
     };
   }
 
@@ -17,7 +17,15 @@ class Board extends React.Component {
         <ListView
           contentContainerStyle={styles.boardContainer}
           dataSource={this.state.dataSource}
-          renderRow={(data) => <Text style={styles.boxText}>{data}</Text>}
+          renderRow={(data) =>
+            <TouchableNativeFeedback>
+              <View>
+                <Text style={styles.boxText}>
+                  {data}
+                </Text>
+              </View>
+            </TouchableNativeFeedback>
+          }
         />
         <View style={styles.lineY1} />
         <View style={styles.lineY2} />
